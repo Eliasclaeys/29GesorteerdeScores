c63ab0a406616d3670539a008fb340965f81f4bf
Elias Claeys <elias.claeys@student.romerocollege.be>
2021-01-13 10:32:30 +0100

    Add project files.

---
+++ GesorteerdeScores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GesorteerdeScores
+{
+    class Program
+    {
+        //5ITN Elias Claeys 13/01/2021
+        /* Opgave:
+         * =========
+         * de gebruiker geeft 5 scores in. vervolgens berekent en toont het 
+           programma het gemiddelde en de scores van hoog naar laag
+         *
+         *ANALYSE:
+         *=========
+         * Maak uw scores[] aan
+         * Herhaal 5 keer zolang i < 6 i++
+         *    Vraag de score (score 1 wordt geplaatst in score[1] enzo.)
+         * Bereken: gemiddelde scores[] = Totaal Scores / Aantal scores
+         * Sorteer: Scores
+         * TOON: gemiddelde
+         * TOON: scores
+         * 
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
